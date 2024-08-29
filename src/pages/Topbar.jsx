04344d1e873af3562817a26a18277ec090f07476
Rahul Lokhande <rlokhande06244@gmail.com>
2024-08-29 10:20:38 +0530

    Changes

--- src/pages/Topbar.jsx
+++ src/pages/Topbar.jsx
@@ -11,7 +11,40 @@ import img1 from "../../public/hospital.png";
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 
+import { useState,useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Table from 'react-bootstrap/Table';
 const Topbar=()=>{
+
+  const [Doc,setDoc]=useState("");
+  const [mydata,setmydata]=useState([])
+  const navigate=useNavigate();
+
+  const handlesearch=()=>{
+    let api=`http://localhost:3000/Patients/?doctor=${Doc}`
+    axios.get(api).then((res)=>{
+         setmydata(res.data)
+    })
+  }
+  
+
+  
+  const ans=mydata.map((key)=>{
+    return(
+      <>
+          <tr>
+            <td>{key.name}</td>
+            <td>{key.city}</td>
+            <td>{key.age}</td>
+            <td>{key.adhar}</td>
+            <td>{key.contact}</td>
+            <td>{key.doctor}</td>
+          </tr>
+      </>
+    )
+  })
+
   return(
     <>
     {/* ============================== Navbar Section ========================== */}
@@ -29,7 +62,7 @@ const Topbar=()=>{
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link as={Link} to="insert" style={{marginRight:"20px"}}>Enter Patient Details</Nav.Link>
+            {/* <Nav.Link as={Link} to="insert" style={{marginRight:"20px"}}>Enter Patient Details</Nav.Link> */}
             <Nav.Link as={Link} to="display" style={{marginRight:"20px"}}>Patients Details</Nav.Link>
             <Nav.Link as={Link} to="search" style={{marginRight:"20px"}}>Search</Nav.Link>
             <NavDropdown title="Appointments" id="navbarScrollingDropdown">
@@ -43,14 +76,46 @@ const Topbar=()=>{
             </NavDropdown>
             
           </Nav>
+ {/* ============================ Search Section =============================== */}
           <Form className="d-flex" style={{marginRight:"50px"}}>
             <Form.Control
               type="search"
               placeholder="Search"
               className="me-2"
               aria-label="Search"
+              value={Doc} onChange={(e)=>{setDoc(e.target.value)}}
+              
             />
-            <Button variant="outline-success">Search</Button>
+            <Button variant="outline-success" onClick={handlesearch} >Search</Button>
+  {/* ================================================================= */}
+        {/* <div>
+          {ans}
+        </div> */}
+        {/* <section style={{display:"block"}}>
+        <div style={{display:"block"}}>
+
+             <b style={{marginLeft:"500px",fontSize:"20px"}}>Search</b>
+             <input style={{marginLeft:"10px",marginTop:"3%",width:"300px"}} type="text" value={Doc} onChange={(e)=>{setDoc(e.target.value)}} />
+             <button onClick={handlesearch} style={{marginLeft:"10px",borderRadius:"10px",backgroundColor:"gray",color:"white",marginBottom:"20px"}}>Search</button>
+        </div> 
+        <div style={{marginLeft:"350px",width:"50%"}}> 
+        <Table striped bordered hover >
+
+           <tr>
+              <th>Patients Name</th>
+              <th>City</th>
+              <th>Age</th>
+              <th>Aadhar No.</th>
+              <th>Doctor</th>
+          </tr>
+    
+         <tbody>
+             {ans}
+         </tbody>
+           </Table>
+         </div>
+        </section> */}
+
           </Form>
         </Navbar.Collapse>
       </Container>
